ea7b8be51241b8b0f611a641db0e6f7e81967a1d
marc5067 <marcelgasperlin2@gmail.com>
2024-06-13 00:14:12 +0200

    Update header_menu.js

--- JS/header_menu.js
+++ JS/header_menu.js
@@ -1,3 +1,19 @@
+function openSidebar() {
+  document.getElementById('sidebar').classList.add('open');
+  document.getElementById('overlay').classList.add('show');
+  document.body.classList.add('no-scroll');
+}
+  
+function closeSidebar() {
+  document.getElementById('sidebar').classList.remove('open');
+  document.getElementById('overlay').classList.remove('show');
+  document.body.classList.remove('no-scroll');
+}
+  
+// Close the sidebar when clicking on the overlay
+document.getElementById('overlay').addEventListener('click', closeSidebar);
+
+//
 function showSideBar() {
     const sidebar = document.querySelector(".sidebar")
     sidebar.style.display = "flex"
